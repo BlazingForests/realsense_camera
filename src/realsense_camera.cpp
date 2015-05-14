14a061167933aa4fe8c9736833efbb90cc8fddfb
ddz <dd@dorabot.com>
2015-05-14 11:51:55 +0800

    use opencv to convert YUV to BGR

--- src/realsense_camera.cpp
+++ src/realsense_camera.cpp
@@ -387,52 +387,6 @@ int processDepth()
 }
 
 
-// This is probably the most acceptable conversion from camera YUYV to RGB
-//
-// Wikipedia has a good discussion on the details of various conversions and cites good references:
-// http://en.wikipedia.org/wiki/YUV
-//
-// Also http://www.fourcc.org/yuv.php
-//
-// What's not clear without knowing more about the camera in question is how often U & V are sampled compared
-// to Y.
-//
-// E.g. YUV444, which is equivalent to RGB, where both require 3 bytes for each pixel
-//      YUV422, which we assume here, where there are 2 bytes for each pixel, with two Y samples for one U & V,
-//              or as the name implies, 4Y and 2 UV pairs
-//      YUV420, where for every 4 Ys, there is a single UV pair, 1.5 bytes for each pixel or 36 bytes for 24 pixels
-
-void yuv2rgb(int y, int u, int v, unsigned char *r, unsigned char *g, unsigned char *b)
-{
-   int r1, g1, b1;
-
-   // replaces floating point coefficients
-   int c = y-16, d = u - 128, e = v - 128;
-
-   // Conversion that avoids floating point
-   r1 = (298 * c           + 409 * e + 128) >> 8;
-   g1 = (298 * c - 100 * d - 208 * e + 128) >> 8;
-   b1 = (298 * c + 516 * d           + 128) >> 8;
-
-   // Computed values may need clipping.
-   if (r1 > 255) r1 = 255;
-   if (g1 > 255) g1 = 255;
-   if (b1 > 255) b1 = 255;
-
-   if (r1 < 0) r1 = 0;
-   if (g1 < 0) g1 = 0;
-   if (b1 < 0) b1 = 0;
-
-//   *r = r1 ;
-//   *g = g1 ;
-//   *b = b1 ;
-
-   *r = b1 ;
-   *g = g1 ;
-   *b = r1 ;
-}
-
-
 void
 processRGBD()
 {
@@ -448,25 +402,17 @@ processRGBD()
         return;
     }
 
-	Mat depth_frame(depth_stream.height, depth_stream.width, CV_8UC1, depth_frame_buffer);
+	cv::Mat depth_frame(depth_stream.height, depth_stream.width, CV_8UC1, depth_frame_buffer);
 
 #ifdef V4L2_PIX_FMT_INZI
-	Mat ir_frame;
-	ir_frame = Mat(depth_stream.height, depth_stream.width, CV_8UC1, ir_frame_buffer);
+	cv::Mat ir_frame(depth_stream.height, depth_stream.width, CV_8UC1, ir_frame_buffer);
 #endif
 
+	cv::Mat rgb_frame_yuv(rgb_stream.height, rgb_stream.width, CV_8UC2, rgb_frame_buffer);
+	cv::Mat rgb_frame;
 
-	Mat rgb_frame(rgb_stream.height, rgb_stream.width, CV_8UC3, rgb_frame_buffer);
-	//YUV 2 RGB
-	int y_temp, y2_temp, u_temp, v_temp;
-	unsigned char *pptr = (unsigned char *)rgb_stream.fillbuf;
-	for(int i=0, newi=0; i<rgb_stream.buflen; i=i+4, newi=newi+6)
-	{
-		y_temp=(int)pptr[i]; u_temp=(int)pptr[i+1]; y2_temp=(int)pptr[i+2]; v_temp=(int)pptr[i+3];
-		yuv2rgb(y_temp, u_temp, v_temp, &rgb_frame_buffer[newi], &rgb_frame_buffer[newi+1], &rgb_frame_buffer[newi+2]);
-		yuv2rgb(y2_temp, u_temp, v_temp, &rgb_frame_buffer[newi+3], &rgb_frame_buffer[newi+4], &rgb_frame_buffer[newi+5]);
-    }
-
+	memcpy(rgb_frame_buffer, rgb_stream.fillbuf, rgb_stream.buflen);
+	cv::cvtColor(rgb_frame_yuv,rgb_frame,CV_YUV2BGR_YUYV);
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr realsense_xyz_cloud (new pcl::PointCloud<pcl::PointXYZ>());
 	realsense_xyz_cloud->width = depth_stream.width;
@@ -530,7 +476,7 @@ processRGBD()
 					int cy = (int)(uvy * rgb_stream.height + 0.5f);
 					if (cx >= 0 && cx < rgb_stream.width && cy >= 0 && cy < rgb_stream.height)
 					{
-						unsigned char *rgb = rgb_frame_buffer + (cx+cy*rgb_stream.width)*3;
+						unsigned char *rgb = rgb_frame.data + (cx+cy*rgb_stream.width)*3;
 						unsigned char r = rgb[2];
 						unsigned char g = rgb[1];
 						unsigned char b = rgb[0];
@@ -841,7 +787,7 @@ int main(int argc, char* argv[])
 
     printf("RealSense Camera is running!\n");
 
-    rgb_frame_buffer = new unsigned char[rgb_stream.width * rgb_stream.height * 3];
+    rgb_frame_buffer = new unsigned char[rgb_stream.width * rgb_stream.height * 2];
     depth_frame_buffer = new unsigned char[depth_stream.width * depth_stream.height];
 
 #ifdef V4L2_PIX_FMT_INZI
